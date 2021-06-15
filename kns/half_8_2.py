0ac20603b44cc4cfbbcd940130e780d20f08d4db
Daiiqi <74845012+Daiiqi@users.noreply.github.com>
2021-06-15 11:44:00 +0800

    1.1.4（kns）
    根据 horikun 1.1.4 的功能，更新了 kns 里的用户脚本

--- kns/half_8_2.py
+++ kns/half_8_2.py
@@ -1,42 +1,8 @@
 """
-8-2 中途返城。是一个添了点东西的用户脚本。
-新的结构使它可以配合现在的 horikun 实现 8-2 图敌人加强点之前就回城，且中途掉了金刀装就会中断挂机，防止造成损失。
+8-2中途返城。
 """
-def go_battle_82_mid():
-    """
-    从本丸或者战中的任何一个点开始，进入无脑loop的循环
-    """
-    global last_map
-    if if_in_home():
-        enter_battle_select()   # 从本丸进入选地图的画面
-        map_select(8, 2, last_map)  # 选择8-2
-        enter_battle_map()      # 进图
-        if not last_map:
-            log(BATTLE_CHAT[8-1][2-1])      # 输出个8-2专用log
-            last_map = True
-    else:   # 不在本丸，在战场里
-        if_82_march = check_rgb(324, 54 , [56 , 120, 27 ]) and check_rgb(946, 83 , [107, 132, 107]) and check_rgb(936, 415, [255, 255, 255])
-        if if_82_march:         # 到了该进军的时候
-            if_82_mid = check_rgb(718, 187, [246, 239, 222]) and check_rgb(718, 179, [136, 120, 112])
-            if_drop_gb = False
-            for i in range(6):
-                for j in range(3):
-                    if if_gold_tama[i][j] and (not check_rgb_rough(MARCH_GB_X[j],MARCH_GB_Y[i],MARCH_GB_RGB,MARCH_GB_ERR)):
-                        if_drop_gb = True
-            if if_82_mid or if_drop_gb:       # 到地方了，要回城了
-                click(932, 440)
-                wait(500)
-                click(415, 377)
-                if if_drop_gb:
-                    win32api.MessageBox(0, "队内第 "+str(i+1)+" 位男士的第 "+str(j+1)+" 个刀装损坏了", "掉蛋了！", win32con.MB_ICONWARNING)
-                    endscript()
-            else:
-                click(631, 449)
-        else:
-            click(631, 449)
-    wait(1000)
+middle=[718, 187, [246, 239, 222]]  # 中途回城条件为：在小地图上显示到了回城点
 
 # 主程序    
 while(1):
-    go_battle_82_mid()
-    
+    go_battle_simple(8,2, middle=middle)
--- kns/normal_1_1.py
+++ kns/normal_1_1.py
@@ -1,5 +1,7 @@
 """
-无限loop 1-1。不建议给兼桑使用。
+可以设定次数地loop 1-1。
+要loop的回数填在limit里，填0为无限循环出阵。
+现在可以给兼桑使用了！
 """
 while(1):
-    go_battle_simple(1,1)
+    go_battle_simple(1,1,limit=3)
